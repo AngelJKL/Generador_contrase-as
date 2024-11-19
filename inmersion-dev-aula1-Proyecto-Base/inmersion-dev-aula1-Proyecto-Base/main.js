d14f2a38f5f6a5609c32d8d51b41e4c9f199cbdf
AngelJKL <117308087+AngelJKL@users.noreply.github.com>
2024-11-19 01:03:45 -0600

    Add files via upload
    generador de contraseña del curso inmersivo Dev en java

---
+++ inmersion-dev-aula1-Proyecto-Base/inmersion-dev-aula1-Proyecto-Base/main.js
@@ -0,0 +1,30 @@
+var cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contraseña = document.getElementById('contrasena');
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01';
+
+function generar(){
+
+    let numeroDigitado = parseInt ( cantidad.value );
+
+    console.log(numeroDigitado);
+    if(numeroDigitado < 8 ){
+    alert("La cantidad de caracteres tiene que ser mayo que 8");
+    }
+    let password = '';
+    for( let i= 0; i <  numeroDigitado; i++  ) {
+
+        let caracterAleatorio = cadenaCaracteres [Math.floor ( Math.random() * cadenaCaracteres.length)];
+            console.log(caracterAleatorio);
+
+            password+=caracterAleatorio;
+
+    }
+    contrasena.value= password;
+
+}
+
+
+
+
+
